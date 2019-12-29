453cf1e290bc417f7c4de845df4221b618abd149
Michael <michaelhooper13@outlook.com>
2019-12-29 21:12:00 +0000

    Set DEBUG=FALSE for heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -53,9 +53,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n6a)0n*694z!c#if08)$l%k#kb&*c2m4*tja%)alu%4id%3&+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["learning-log-michaelh.herokuapp.com"]
 
 
 # Application definition
